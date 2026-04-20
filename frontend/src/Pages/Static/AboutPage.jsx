957aa7829c4dec846c397ee210a6e5d9083276b1
pullangari-vedaprakash <vedaprakash8341@gmail.com>
2026-04-20 13:40:17 +0530

    error commit

--- frontend/src/Pages/Static/AboutPage.jsx
+++ frontend/src/Pages/Static/AboutPage.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { PawPrint, Heart, Users, ShieldCheck, Sparkles } from "lucide-react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import x from ;
 
 const AboutPage = () => {
   return (
