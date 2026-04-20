b39af9b9cbab0d7349b87470e50b31eac59ad4fb
G1kumar1808 <jeevankumar180805@gmail.com>
2026-04-20 12:11:02 +0530

    password length to 8

--- backend/src/controller/authControllers.js
+++ backend/src/controller/authControllers.js
@@ -23,7 +23,7 @@ export const signup = async (req, res, next) => {
       return res.status(400).json({ message: "Invalid email format" });
     }
 
-    if (password.length < 6) {
+    if (password.length < 8) {
       return res
         .status(403)
         .json({ message: "Password Must have 6 characters" });
