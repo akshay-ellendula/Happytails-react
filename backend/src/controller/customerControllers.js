2241d4bc7943bb824ada41fb5152c2ddbaa6d315
G1kumar1808 <jeevankumar180805@gmail.com>
2026-04-20 12:32:25 +0530

    starting user edit any

--- backend/src/controller/customerControllers.js
+++ backend/src/controller/customerControllers.js
@@ -60,7 +60,7 @@ export const putCustomer = async (req, res, next) => {
         .json({ success: false, message: "Only @gmail.com allowed" });
     }
 
-    if (phoneNumber && !/^[6-9]\d{9}$/.test(String(phoneNumber))) {
+    if (phoneNumber && !/^\d{10}$/.test(String(phoneNumber))) {
       return res
         .status(400)
         .json({ success: false, message: "Phone number must be 10 digits" });
